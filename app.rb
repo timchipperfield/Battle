e25bcf6664d20fc048b6f70c2d2a62565f007e8d
awye765 <awye765@googlemail.com>
2016-06-08 16:43:52 +0100

    Added functionality to view player 1 HP and refactored test code using a helper

--- app.rb
+++ app.rb
@@ -1,16 +1,28 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
   post '/names' do
-  	@player_1_name = params[:player_1_name]
-  	@player_2_name = params[:player_2_name]
-  	erb :play
+  	session[:player_1_name] = params[:player_1_name]
+  	session[:player_2_name] = params[:player_2_name]
+  	# erb :play
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
+    erb :play
   end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
+  set :session_secret, 'super secret'
+
 end
