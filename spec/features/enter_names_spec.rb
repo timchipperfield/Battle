d27709a651e52b38462d8aaa65be4dbb5194ae65
timchipperfield <timchipperfield@gmail.com>
2016-06-08 13:04:41 +0100

    add  get names feature

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,15 +1,16 @@
+require 'spec_helper'
 
 
 feature 'enter names' do
 
 	scenario 'submitting names' do
-		
+
 		visit('/')
-		fill_in('Player One', with: 'Tim')
-		fill_in('Player Two', with: 'Graham')
-		click('submit')
+		fill_in :player_1_name, with: 'Tim'
+		fill_in :player_2_name, with: 'Graham'
+		click_button('submit')
 		expect(page).to have_content('Tim vs. Graham')
-	
+
 	end
 
 
