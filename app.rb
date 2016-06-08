af61fc8a2be55327da9bf8cbdf69106cc7617e05
awye765 <awye765@googlemail.com>
2016-06-08 18:09:38 +0100

    Added player class and unit test - needs fixing

--- app.rb
+++ app.rb
@@ -2,33 +2,32 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
-  enable :sessions
+  # enable :sessions
 
   get '/' do
     erb :index
   end
 
   post '/names' do
-  	session[:player_1_name] = params[:player_1_name]
-  	session[:player_2_name] = params[:player_2_name]
-  	# erb :play
+    $player_1 = Player.new(params[:player_1_name])
+    $player_2 = Player.new(params[:player_2_name])
     redirect '/play'
   end
 
   get '/play' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
     erb :play
   end
 
   get '/attack' do
-  	@player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
     erb :attack
   end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
-  set :session_secret, 'super secret'
+  # set :session_secret, 'super secret'
 
 end
